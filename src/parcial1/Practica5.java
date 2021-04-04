07b07b29659217969286e1c1cd1103bc90fd7af8
Juan Pablo <eljuan1820@gmail.com>
2021-04-03 22:54:21 -0600

    practica 5

--- src/parcial1/Practica5.java
+++ src/parcial1/Practica5.java
@@ -1,6 +1,32 @@
 
 package parcial1;
-
+import java.util.Scanner;
 public class Practica5 {
+    
+
+     private int resultado;
+    private double num[];
+    private double suma,Promedio;
+    
+    public void Promediar(){
+    Scanner leer = new Scanner(System.in);
+        num = new double[10];
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Introdusca 10 numeros a promediar");
+            num [i] = leer.nextInt();
+            
+        }
+        suma = num[0]+num[1]+num[2]+num[3]+num[4]+num[5]+num[6]+num[7]+num[8]+num[9];
+        Promedio = suma/10;
+        System.out.println("El promedio es de" +Promedio);
+    }
+    public static void main(String[] args) {
+   Practica5 eje = new Practica5();
+   eje.Promediar();
+    }
+    
+
+
     
 }
